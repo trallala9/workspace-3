208f8997e8327f2431bf2967e4e939a1a904f235
Karolina Siwkowska <trallala00@gmail.com>
2017-12-29 16:17:48 +0100

    Add sample code

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
 // scripts.js
 
-var name
-
+var a = 5
+var h = 10
+var triangleArea = 2
+triangleArea = a * h / 2
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
 //other comment
 
 //lovely comment
