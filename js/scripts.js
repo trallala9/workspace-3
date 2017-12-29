382725e3b7ff464c45ef07b364179f42b86bff7f
Karolina Siwkowska <trallala00@gmail.com>
2017-12-29 16:23:30 +0100

    Make some chcnges in code

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
 // scripts.js
 
-var a = 5
-var h = 10
-var triangleArea = 2
-triangleArea = a * h / 2
+var a = 5;
+var h = 10;
+var triangleArea = a * h / 2;
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
 //other comment
 
